5ff6d5e7d8933ce55aeb4d5c8415765676811f92
yjhn <andrius.puksta@gmail.com>
2023-04-15 16:50:19 +0300

    add cli argument parsing

---
+++ src/arguments.rs
@@ -0,0 +1,57 @@
+use std::ffi::OsStr;
+
+use clap::{Parser, ValueEnum};
+
+use crate::config;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+pub enum DuplicateHandling {
+    /// Panic.
+    Panic,
+    /// Ignore (does not run duplicate benchmarks)
+    Skip,
+    /// Use a different name (append a number) for our benchamrk results file.
+    SwitchName,
+    /// Overwrite old file.
+    Overwrite,
+}
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    #[arg(short, long, required = true, num_args(1..))]
+    /// TSP problem definition files.
+    pub files: Vec<String>,
+
+    #[arg(long, required = false, default_value_t = config::ALPHA)]
+    pub alpha: f32,
+
+    #[arg(long, required = false, default_value_t = config::BETA)]
+    pub beta: f32,
+
+    #[arg(long, required = false, default_value_t = config::PERSISTENCE)]
+    pub persistence: f32,
+
+    #[arg(long, required = false, default_value_t = config::Q)]
+    pub q: f32,
+
+    #[arg(long, required = false, default_value_t = config::INITIAL_TRAIL_INTENSITY)]
+    pub init_intensity: f32,
+
+    #[arg(short, long, default_value_t = config::MAX_ITERATIONS)]
+    /// Maximum number of generations for obtaining the optimal solution.
+    pub max_iterations: u32,
+
+    #[arg(short, long, default_value_t = config::REPEAT_TIMES)]
+    /// Number of times to repeat the benchmark.
+    pub bench_repeat_times: u32,
+
+    #[arg(long, required = false, default_value_t = config::RESULTS_DIR.to_owned())]
+    pub bench_results_dir: String,
+
+    #[arg(short, long, required = false, num_args(1..))]
+    pub population_sizes: Vec<u32>,
+
+    #[arg(value_enum, long, required = false, default_value_t = DuplicateHandling::Panic)]
+    /// What to do if existing benchmark results files are found.
+    pub dup: DuplicateHandling,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+mod arguments;
+
+use crate::arguments::Args;
+use clap::Parser;
+
+mod config {
+    // α: the relative importance of the trail, α ≥ 0
+    pub const ALPHA: f32 = 0.5;
+    // β: the relative importance of the visibility, β ≥ 0
+    pub const BETA: f32 = 0.5;
+    // ρ: trail persistence, 0≤ρ<1 (1-ρ can be interpreted as trail evaporation)
+    pub const PERSISTENCE: f32 = 0.5;
+    // Q: a constant related to the quantity of trail laid by ants
+    pub const Q: f32 = 0.5;
+
+    pub const POPULATION_SIZES: [u32; 5] = [8, 16, 32, 64, 128];
+    pub const MAX_ITERATIONS: u32 = 1000;
+    // Initial intensity of all trails.
+    pub const INITIAL_TRAIL_INTENSITY: f32 = 0.2;
+
+    // Benchmark repeat times.
+    pub const REPEAT_TIMES: u32 = 10;
+    // Benchmark results directory.
+    pub const RESULTS_DIR: &str = "results";
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = arguments::Args::parse();
+    dbg!(&args);
+    let Args {
+        files,
+        alpha,
+        beta,
+        persistence,
+        q,
+        init_intensity,
+        max_iterations,
+        bench_repeat_times,
+        bench_results_dir,
+        population_sizes,
+        dup,
+    } = args;
 }
