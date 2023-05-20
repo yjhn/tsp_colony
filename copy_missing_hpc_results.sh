3194f5a4f77e941cf4f8fefa149e07532966b4c5
yjhn <andrius.puksta@gmail.com>
2023-05-20 11:16:29 +0300

    make results download script work from anywhere

--- copy_missing_hpc_results.sh
+++ copy_missing_hpc_results.sh
@@ -2,18 +2,20 @@
 
 set -eu
 
-echo "Removing write permission fro mdestionation files"
+results_dir="$(dirname $(realpath $0))/hpc_results"
+
+echo "Removing write permission from destination files"
 # Remove write permission on existing results files, this way scp will not overwrite them.
 # Idea from: https://unix.stackexchange.com/a/51932
-chmod -w hpc_results/*.json
+chmod -w "${results_dir}"/*.json
 
 set +e -x
 
 # Silence scp complaints that it is unable to write some files.
-scp -r 'hpc.mif.vu.lt:bak/results/*' hpc_results
+scp -r 'hpc.mif.vu.lt:bak/results/*' "${results_dir}"
 
 set -e +x
 
 echo "Restoring write permission for destination files"
 # Restore write permissions.
-chmod +w hpc_results/*.json
+chmod +w "${results_dir}"/*.json
