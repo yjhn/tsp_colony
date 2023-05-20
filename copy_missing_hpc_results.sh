982aa98d763473f6935388a9904fefaca9323c4e
yjhn <andrius.puksta@gmail.com>
2023-05-20 11:42:02 +0300

    add filtering explanation

--- copy_missing_hpc_results.sh
+++ copy_missing_hpc_results.sh
@@ -12,6 +12,7 @@ chmod -w "${results_dir}"/*.json
 set +e -x
 
 # Silence scp complaints that it is unable to write some files.
+# Filter output lines by switching stdout and stderr: https://stackoverflow.com/a/2381643
 scp -r 'hpc.mif.vu.lt:bak/results/*' "${results_dir}" 3>&1 1>&2 2>&3 3>&- | grep -vx "scp: open local .*: Permission denied."
 
 set -e +x
