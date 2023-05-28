9ab480a38cc5937870df46669cc9d3245a2ff52a
yjhn <andrius.puksta@gmail.com>
2023-05-28 21:50:36 +0300

    add CABC graphs, start adding qCABC graphs

--- plot/included_graphs.sh
+++ plot/included_graphs.sh
@@ -4,83 +4,126 @@
 # Pašalina senus grafikus jeigu tokie yra.
 
 PLOT_DIR=../../bakalaurinis/plot
+# PLOT_DIR=plot
 BENCH_RESULTS_DIR=../results
 # For putting two side-by-side.
 STD_SCALE=0.45
 # For one taking up full page width.
 LARGE_SCALE=0.8
+# FORMAT=png
 FORMAT=pgf
-PREAMBLE="python plot.py -p ${PLOT_DIR} -d ${BENCH_RESULTS_DIR} -f ${FORMAT}"
+PRE="python plot.py -p ${PLOT_DIR} -d ${BENCH_RESULTS_DIR} -f ${FORMAT} -s ${STD_SCALE}"
 
-echo "Removing plot dir ${PLOT_DIR@Q}"
-rm -Ir "${PLOT_DIR}"
-mkdir "${PLOT_DIR}"
+# echo "Removing plot dir ${PLOT_DIR@Q}"
+# rm -Ir "${PLOT_DIR}"
+# mkdir "${PLOT_DIR}"
 
 # Jeigu kas nors nepavyksta, baigiam.
 set -eux
 
-${PREAMBLE} -a PACO -t lin318 -c 1 2 4 6 8 -e 8 32 --y-bottom 7 --y-top 20 --capital-ls 2 -k gens_diff_excg
-${PREAMBLE} -a CABC -c 1 2 4 6 8 -e 8 32 --capital-ls 2 -k gens_diff_excg
+# # CABC algoritmas
+# # L daro įtaką rezultatams, bet praktiškai random, todėl rezultatai rodomi su L = 2
+
+# # gens_diff_excg
+# # kroA100 ir eil51 grafikai neįdomūs
+# # Visur išskyrus lin318 D_m = 32 gerėja lėčiausiai, bet po to pralenkia kitus D_m.
+# # Su lin318, kuo mažesnis D_m, tuo geriau veikia.
+# ${PRE} -a CABC -t lin318 -c 2 4 -e 2 4 8 32 --population-sizes 40 --capital-ls 3 -k gens_diff_excg --cond-y-top 3
+# ${PRE} -a CABC -t kroA200 -c 2 4 -e 2 4 8 32 --population-sizes 40 --capital-ls 3 -k gens_diff_excg --cond-y-top 3
+# ${PRE} -a CABC -t eil76 -c 2 4 -e 2 4 8 32 --population-sizes 40 --capital-ls 3 -k gens_diff_excg --cond-y-top 3
+
+
+# # cores_diff_test_cases
+# # Gal šito nedėsiu. Nieko įdomaus be to, kad rezultatai menkai gerėja su daugiau branduolių.
+# # TODO: grafikas, parodantis kartas ir branduolius su fiksuotu L.
+
+
+# # cores_diff_gens
+# # Šitas parodo, kad daugiau branduolių padeda žymiai greičiau gerėti iš pradžių, bet
+# # optimalaus sprendinio surasti iš esmės nepadeda.
+# ${PRE} -a CABC -k cores_diff_gens -t lin318 -c 1 2 4 6 8 --population-sizes 40 -e 2 --capital-ls 3
+# # Su mažesniais miestų skaičiais labai mažai naudos iš > 2 branduolių.
+# ${PRE} -a CABC -k cores_diff_gens -t kroA200 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 2
+# # Galima parodyti ir kroA100 čia, bet jis neįdomus, nes per lengvas.
+# ${PRE} -a CABC -k cores_diff_gens -t kroA100 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 2
+# # eil76 elgiasi keistai: du branduoliai duoda prastesnius rezultatus nei 1
+# # eil51 rezultatai panašūs
+# ${PRE} -a CABC -k cores_diff_gens -t eil76 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 2
+
+
+# # gens_diff_popsizes
+# # Kaip ir galima tikėtis, didesnė populiacija duoda geresnius rezultatus.
+# # Tačiau ne visada. Kartais su skirtingais L rezultatai apsiverčia.
+# ${PRE} -a CABC -k gens_diff_popsizes -t lin318 d198 -c 6 8 --population-sizes 20 40 80 -e 8 --capital-ls 2 --cond-y-top 5
+
+
+# # cores_diff_popsizes
+# # TODO: nebaigti testai
+# ${PRE} -a CABC -k cores_diff_popsizes -t lin318 d198 -c 1 2 4 6 8 --population-sizes 20 40 80 -e 8 32 --capital-ls 2
+
+
+# # gens_diff_cls
+# # L įtaka rezultatams atsitiktinė.
+# ${PRE} -a CABC -k gens_diff_cls -t lin318 d198 -c 6 --population-sizes 40 -e 8 --capital-ls 2 3 4
+# ${PRE} -a CABC -k gens_diff_cls -t eil76 -c 1 4 --population-sizes 40 -e 8 --capital-ls 2 3 4 --cond-y-top 3
+
+
+# # cores_diff_cls
+# # Vėlgi, L įtaka atsitiktinė.
+# ${PRE} -a CABC -k cores_diff_cls -t lin318 -c 1 2 4 6 8 --population-sizes 40 -e 8 --capital-ls 2 3 4
+# ${PRE} -a CABC -k cores_diff_cls -t kroA200 d198 eil76 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 2 3 4
+
+
+# Išvados: gal reikėjo testuoti ir su daugiau miestų? Nes su mažiau miestų rezultatai beveik random.
+
+
+# qCABC algoritmas
+
+# gens_diff_excg
+# kroA100 ir eil51 grafikai neįdomūs
+# Visur išskyrus lin318 D_m = 32 gerėja lėčiausiai, bet po to pralenkia kitus D_m.
+# Su lin318, kuo mažesnis D_m, tuo geriau veikia.
+${PRE} -a CABC -t lin318 -c 2 4 -e 2 4 8 32 --population-sizes 40 --capital-ls 3 -k gens_diff_excg --cond-y-top 3
+${PRE} -a CABC -t kroA200 -c 2 4 -e 2 4 8 32 --population-sizes 40 --capital-ls 3 -k gens_diff_excg --cond-y-top 3
+${PRE} -a CABC -t eil76 -c 2 4 -e 2 4 8 32 --population-sizes 40 --capital-ls 3 -k gens_diff_excg --cond-y-top 3
+
+# cores_diff_gens
+${PRE} -a qCABC -k cores_diff_gens -t lin318 -c 1 2 4 6 8 --population-sizes 40 -e 8 --capital-ls 3
+${PRE} -a qCABC -k cores_diff_gens -t eil51 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 4
+${PRE} -a qCABC -k cores_diff_gens -t d198 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 4
+${PRE} -a qCABC -k cores_diff_gens -t kroA200 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 2
+
+
+# gens_diff_popsizes
+# Kaip ir galima tikėtis, didesnė populiacija duoda geresnius rezultatus.
+# Tačiau ne visada. Kartais su skirtingais L rezultatai apsiverčia.
+${PRE} -a qCABC -k gens_diff_popsizes -t lin318 d198 -c 6 8 --population-sizes 20 40 80 -e 8 --capital-ls 2 --cond-y-top 5
+
+
+# cores_diff_popsizes
+# TODO: nebaigti testai
+${PRE} -a qCABC -k cores_diff_popsizes -t lin318 d198 -c 1 2 4 6 8 --population-sizes 20 40 80 -e 8 32 --capital-ls 2
+
+
+# gens_diff_cls
+# L įtaka rezultatams atsitiktinė.
+${PRE} -a qCABC -k gens_diff_cls -t lin318 d198 -c 6 --population-sizes 40 -e 8 --capital-ls 2 3 4
+${PRE} -a qCABC -k gens_diff_cls -t eil76 -c 1 4 --population-sizes 40 -e 8 --capital-ls 2 3 4 --cond-y-top 3
+
+
+# cores_diff_cls
+# Vėlgi, L įtaka atsitiktinė.
+${PRE} -a qCABC -k cores_diff_cls -t lin318 -c 1 2 4 6 8 --population-sizes 40 -e 8 --capital-ls 2 3 4
+${PRE} -a qCABC -k cores_diff_cls -t kroA200 d198 eil76 -c 1 2 4 6 8 --population-sizes 40 -e 32 --capital-ls 2 3 4
 
 
 
 
-# Grafikai tokie patys kaip straipsnyje (D_m palyginimas).
-YTOP3OPT=3.0
-YBOTTOM3OPT=0.75
-YTOP2OPT=8.9
-YBOTTOM2OPT=5.2
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga3opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP3OPT --y-bottom $YBOTTOM3OPT -e 4
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga3opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP3OPT --y-bottom $YBOTTOM3OPT -e 8
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga3opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP3OPT --y-bottom $YBOTTOM3OPT -e 16
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga3opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP3OPT --y-bottom $YBOTTOM3OPT -e 32
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP2OPT --y-bottom $YBOTTOM2OPT -e 4
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP2OPT --y-bottom $YBOTTOM2OPT -e 8
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP2OPT --y-bottom $YBOTTOM2OPT -e 16
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -p $PLOT_DIR -f pgf -k cores_diff_test_cases -s 0.45 --y-top $YTOP2OPT --y-bottom $YBOTTOM2OPT -e 32
+# PACO algoritmas
 
 
-# Grafikai, parodantys kaip einant kartoms trumpėja maršrutas.
-# Palygina skirtingus D_m
-# Tik cga2opt att532 ir cga3opt rat783, nes kitaip per daug grafikų.
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 -c 1 2 4 8 --population-sizes 128 -e 4 8 16 32 -p $PLOT_DIR -f pgf -k gens_diff_excg -s 0.45
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga3opt -t rat783 -c 1 2 4 8 --population-sizes 128 -e 4 8 16 32 -p $PLOT_DIR -f pgf -k gens_diff_excg -s 0.45
 
 
-# Palyginimas tarp skirtingų populiacijos dydžių (cga2opt).
-# Grupuojama P = 2 4 8 16 į vieną ir P = 32 64 128 256 į kitą.
-# TODO: cga3opt
+# Visi algoritmai
 
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 2 4 8 16 -p $PLOT_DIR -f pgf -k cores_diff_popsizes -s 0.45 -e 4
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 32 64 128 256 -p $PLOT_DIR -f pgf -k cores_diff_popsizes -s 0.45 -e 4
-
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t pr1002 -c 1 2 4 8 --population-sizes 2 4 8 16 32 64 128 256 -p $PLOT_DIR -f pgf -k cores_diff_popsizes -s 0.8 -e 4
-
-
-# Skirtingų populiacijos dydžių palyginimas, kartos.
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -t att532 pr1002 -c 8 --population-sizes 2 4 8 16 32 64 128 256 -e 4 -p $PLOT_DIR -f pgf -k gens_diff_popsizes -s 0.8
-
-
-# Geriausių individų apsikeitimo tarp branduolių santykinis laikas.
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt -p $PLOT_DIR -f pgf -k relative_times -s 0.8
-
-
-# Kartų skaičiaus įtaka skirtingiems atvejams.
-
-python plot.py -d $BENCH_RESULTS_DIR -a cga2opt cga3opt -t att532 gr666 rat783 pr1002 -c 1 2 4 8 --population-sizes 128 -e 4 -p $PLOT_DIR -f pgf -k cores_diff_gens -s 0.45
-
-python plot.py -d ../cga_100000_gens -a cga -t att532 -c 1 2 4 --population-sizes 128 -e 4 -p $PLOT_DIR -f pgf -k cores_diff_gens -s 0.8 --gens-start 19999 --gens-step 20000 --diff-type times -g 100000
+# cores-diff-algos
